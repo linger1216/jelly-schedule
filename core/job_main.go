c1322d25d8cf94e2d8533fe00376d820a70b5c7e
lid.guan <lid.guan@gmail.com>
2020-09-01 15:44:54 +0800

    add mapstructure for load uer config

--- core/job_main.go
+++ core/job_main.go
@@ -2,11 +2,32 @@ package core
 
 import (
 	"github.com/linger1216/jelly-schedule/utils"
+	"github.com/mitchellh/mapstructure"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 )
 
 const DefaultConfigFilename = "/etc/config/schedule_config.yaml"
+
+func LoadUserConfig(field string, obj interface{}) error {
+	configFilename := kingpin.Flag("conf", "config file name").Short('c').Default(core.DefaultConfigFilename).String()
+	kingpin.Version("0.1.0")
+	kingpin.Parse()
+
+	buf, err := readFileContent(*configFilename)
+	if err != nil {
+		return err
+	}
+
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		return err
+	}
+	return mapstructure.Decode(m[field], obj)
+}
 
 func StartClientJob(job Job) {
 	configFilename := kingpin.Flag("conf", "config file name").Short('c').Default(DefaultConfigFilename).String()
@@ -31,4 +52,14 @@ func StartClientJob(job Job) {
 	NewJobServer(etcd, id, job)
 	go utils.InterruptHandler(end)
 	<-end
+}
+
+func readFileContent(filename string) ([]byte, error) {
+	obj, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	buf, err := ioutil.ReadAll(obj)
+	_ = obj.Close()
+	return buf, err
 }
