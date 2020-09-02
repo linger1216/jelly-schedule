59c44d7c34ea0ea8d13e85cab86596936ef5d643
lid.guan <lid.guan@gmail.com>
2020-09-02 18:36:07 +0800

    并行参数会用最近一次的内容来填充 原因是s3download or work 这种模式下, work也希望收到request的指定

--- core/job_parallel.go
+++ core/job_parallel.go
@@ -31,6 +31,8 @@ func (s *ParallelJob) Progress() int {
 	return int(s.progress.Load())
 }
 
+// 参数会用最近一次的内容来填充
+
 func (s *ParallelJob) Exec(ctx context.Context, req string) (string, error) {
 
 	size := len(s.jobs)
@@ -42,12 +44,13 @@ func (s *ParallelJob) Exec(ctx context.Context, req string) (string, error) {
 	var rawErrors Errors
 	paras := make([]string, len(s.jobs))
 	wg := sync.WaitGroup{}
+
+	var para string
 	for i := range s.jobs {
 		wg.Add(1)
 		go func(pos int) {
 			defer wg.Done()
 			defer s.progress.Add(int32(100 / len(s.jobs)))
-			var para string
 			if pos < len(reqs) {
 				para = reqs[pos]
 			}
