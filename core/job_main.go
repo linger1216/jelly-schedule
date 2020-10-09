99a80fa2124b61507b86251ec219f54ea0406b61
lid.guan <lid.guan@wayz.ai>
2020-10-09 15:45:50 +0800

    更改了读取配置的方式, 针对client

--- core/job_main.go
+++ core/job_main.go
@@ -10,12 +10,20 @@ import (
 
 const DefaultConfigFilename = "/etc/config/schedule_config.yaml"
 
-func LoadUserConfig(field string, obj interface{}) error {
-	configFilename := kingpin.Flag("conf", "config file name").Short('c').Default(DefaultConfigFilename).String()
+var (
+	configFilename *string
+	yamlReader     *config.YamlReader
+)
+
+func init() {
+	configFilename = kingpin.Flag("conf", "config file name").Short('c').Default(DefaultConfigFilename).String()
 	kingpin.Version("0.1.0")
 	kingpin.Parse()
-	yaml := config.NewYamlReader(*configFilename)
-	return yaml.ScanKey(field, obj)
+	yamlReader = config.NewYamlReader(*configFilename)
+}
+
+func LoadUserConfig(field string, obj interface{}) error {
+	return yamlReader.ScanKey(field, obj)
 }
 
 func StartClientJob(job Job) {
@@ -24,20 +32,20 @@ func StartClientJob(job Job) {
 	kingpin.Version("0.1.0")
 	kingpin.Parse()
 
-	config, err := LoadScheduleConfig(*configFilename)
+	conf, err := LoadScheduleConfig(*configFilename)
 	if err != nil {
 		panic(err)
 	}
 
-	if len(config.Job.Host) > 0 {
-		err = os.Setenv(sys.SERVICE_HOST, config.Job.Host)
+	if len(conf.Job.Host) > 0 {
+		err = os.Setenv(sys.SERVICE_HOST, conf.Job.Host)
 		if err != nil {
 			panic(err)
 		}
 	}
-	id, _ := config.Job.Ids[job.Name()]
+	id, _ := conf.Job.Ids[job.Name()]
 	end := make(chan error)
-	etcd := NewEtcd(&config.Etcd)
+	etcd := NewEtcd(&conf.Etcd)
 	NewJobServer(etcd, id, job)
 	go sys.InterruptHandler(end)
 	<-end
